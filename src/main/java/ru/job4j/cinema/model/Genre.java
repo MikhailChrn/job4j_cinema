f14a767904e7debc59add44cc698196035b01139
MikhailChrn <mikhail.cherneyev@yandex.ru>
2025-01-08 16:41:48 +0400

    Add data model classes for database handling.

---
+++ src/main/java/ru/job4j/cinema/model/Genre.java
@@ -0,0 +1,35 @@
+package ru.job4j.cinema.model;
+
+import java.util.Map;
+
+public class Genre {
+
+    public static final Map<String, String> COLUMN_MAPPING = Map.of(
+            "id", "id",
+            "name", "name"
+    );
+
+    private int id;
+
+    private String name;
+
+    public Genre(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
