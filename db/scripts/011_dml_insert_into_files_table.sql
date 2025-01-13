9604a55972a640780e892be30da41c2a15c60fc5
MikhailChrn <mikhail.cherneyev@yandex.ru>
2025-01-13 19:04:19 +0400

    Add film SQL_script to fill database.

---
+++ db/scripts/011_dml_insert_into_files_table.sql
@@ -0,0 +1,14 @@
+INSERT INTO files
+VALUES
+(1, '01_astral_2024_horror_18+_90min.png', 'files/01_astral_2024_horror_18+_90min.png'),
+(2, '02_wizzard_2025_action_6+_104min.png', 'files/02_wizzard_2025_action_6+_104min.png'),
+(3, '03_illusion_2024_action_18+_100min.png', 'files/03_illusion_2024_action_18+_100min.png'),
+(4, '04_knock_2025_comedy_12+_91min.png', 'files/04_knock_2025_comedy_12+_91min.png'),
+(5, '05_tellme_2023_drama_18+_116min.png', 'files/05_tellme_2023_drama_18+_116min.png'),
+(6, '06_stories_2023_comedy_0+_44min.png', 'files/06_stories_2023_comedy_0+_44min.png'),
+(7, '07_wedding_2023_comedy_12+_105min.png', 'files/07_wedding_2023_comedy_12+_105min.png'),
+(8, '08_sinister_2023_horror_18+_103min.png', 'files/08_sinister_2023_horror_18+_103min.png'),
+(9, '09_finist_2025_action_6+_112min.png', 'files/09_finist_2025_action_6+_112min.png'),
+(10, '10_megalopolis_2024_fiction_18+_133min.png', 'files/10_megalopolis_2024_fiction_18+_133min.png'),
+(11, '11_dayofweek_2024_drama_18+_85min.png', 'files/11_dayofweek_2024_drama_18+_85min.png'),
+(12, '12_spell_2024_horror_18+_81min.png', 'files/12_spell_2024_horror_18+_81min.png');
